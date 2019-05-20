e459d0795b2062084e6fa1975487d94f15b82253
Roberto Ribeiro <robertov82008@gmail.com>
2019-05-20 15:13:43 -0300

    4 Blade Templating

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('content')
+    <h1>About us</h1>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla bibendum lectus in tortor iaculis, vel dapibus mauris
+        euismod. Vivamus convallis libero ut justo laoreet vestibulum. Nulla varius a nisl at consequat. Aenean at iaculis
+        diam, non lobortis massa. Pellentesque neque orci, sollicitudin sit amet velit sit amet, pharetra pellentesque est.
+        Phasellus pharetra diam ac erat tristique, eget efficitur massa auctor. Sed et commodo nisl, vel fringilla diam.
+        Duis rutrum ante eu libero placerat, luctus laoreet mi porttitor. Nunc feugiat lorem a sem blandit, et hendrerit
+        velit venenatis. Suspendisse cursus sed lorem sit amet ultricies. Nulla nec ipsum non massa volutpat luctus nec ut
+        mauris. Cras dignissim molestie vestibulum. Curabitur id semper dui. Donec sed sollicitudin dui. Donec non pulvinar
+        ligula, et convallis tortor. Sed vel ex id orci porta posuere.
+    </p>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,5 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-    </head>
-    <body>
-        <h1>Here we go!!!</h1>
+@extends('layout')
 
-        <p><a href="/contact">Contact</a> us to learn more.</p>
-    </body>
-</html>
+@section('content')
+    <h1>My First Website!</h1>
+@endsection
