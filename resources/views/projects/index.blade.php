74d916abf98fc8adea9a6c287e3b6814651dd3e0
Roberto Ribeiro <robertov82008@gmail.com>
2019-06-04 17:26:05 -0300

    8 Eloquent Namespacing and MVC

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+
+    <body>
+        <h1>Projects</h1>
+
+        @foreach($projects as $project)
+            <li>{{ $project->title }}</li>
+        @endforeach
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PagesController@home');
 Route::get('/about', 'PagesController@about');
 Route::get('/contact', 'PagesController@contact');
+
+Route::get('/projects', 'ProjectsController@index');
