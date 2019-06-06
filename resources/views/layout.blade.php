5c95121fb04627108b979a8a431f2daeafea1d8c
Roberto Ribeiro <robertov82008@gmail.com>
2019-06-06 09:28:51 -0300

    12 Faking PATCH and DELETE Form Requests

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,20 +2,15 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
         <title>@yield('title', 'Laracasts')</title>
     </head>
 
     <body>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About Us</a></li>
-            <li><a href="/contact">Contact</a></li>
-        </ul>
-
-        @yield('content')
-
+        <div class="container">
+            @yield('content')
+        </div>
     </body>
 </html>
