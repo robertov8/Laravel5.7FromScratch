49b3453b767d19582fb31fb4aa54d037d7b879c6
Roberto Ribeiro <robertov82008@gmail.com>
2019-06-06 11:16:55 -0300

    14 Cleaner Controllers and Mass Assignment Concerns

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,7 +3,13 @@
 @section('content')
     <h1 class="title">Projects</h1>
 
-    @foreach($projects as $project)
-        <li>{{ $project->title }}</li>
-    @endforeach
+    <ul>
+        @foreach($projects as $project)
+            <li>
+                <a href="/projects/{{ $project->id }}">
+                    {{ $project->title }}
+                </a>
+            </li>
+        @endforeach
+    </ul>
 @endsection
